a447155bd09a36b340240ccb95b0e2a8e5dba458
edbrodie <31818910+edbrodie@users.noreply.github.com>
2017-09-18 03:28:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 console.log("Up and running!");
-var cardOne= "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+	
+	cardsInPlay.push(cardOne);
 
 console.log("User flipped " + cardOne);
 
+var cardTwo = cards[2];
+
+	cardsInPlay.push(cardTwo);
+
 console.log("User flipped " + cardTwo);
 
-console.log("User flipped " + cardThree);
 
-console.log("User flipped " + cardFour);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("you found a match!");
+	} else {
+	alert("Sorry, try again");
+	}
+}
